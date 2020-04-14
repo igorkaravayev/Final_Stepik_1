eb3d01fd682eee2c47d517467d4de6a9fccdbc24
igorkaravayev <62888986+igorkaravayev@users.noreply.github.com>
2020-04-14 19:23:39 +0300

    Step_4_2_4

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,8 @@
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-def test_check_basket_button_exists(browser):
-    browser.get(link)
-    message = browser.find_element_by_css_selector(".btn-add-to-basket")
-    assert "Añadir al carrito" in message.text
+from .pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
