246426e6e5743da61c0675c3b85c8ab93a276ed5
igorkaravayev <62888986+igorkaravayev@users.noreply.github.com>
2020-04-22 00:24:44 +0300

    Step_4_3_14_Final

--- test_main_page.py
+++ test_main_page.py
@@ -6,19 +6,6 @@ import math
 import time
 
 
-
-def test_setup(browser):
-        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-        page = LoginPage(browser, link)
-        page.open()
-        email = str(time.time()) + "@fakemail.org"
-        password = str(time.time()) + "Abc"
-        page.register_new_user(email, password)
-        page.should_be_authorized_user()
-        page.logout_user(browser)
-        time.sleep(5)
-        page.should_be_login_page(browser)
-
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = BasePage(browser, link)
--- test_product_page.py
+++ test_product_page.py
@@ -7,14 +7,14 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
  
-
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_optened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = BasePage(browser, link)
     page.open()
     page.go_to_basket_page()    
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
