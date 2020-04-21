60922f93b1009824ad00722910fb56e05b09682e
igorkaravayev <62888986+igorkaravayev@users.noreply.github.com>
2020-04-22 01:36:30 +0300

    Final_Step

--- test_product_page.py
+++ test_product_page.py
@@ -48,8 +48,9 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.should_disappeared_success_message() # Negativ test for an acspected disappeared success after add product
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
-def test_guest_can_add_product_to_basket(self, browser, promo_offer):
+def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = PromoOffer(browser, link)
     page.open()
